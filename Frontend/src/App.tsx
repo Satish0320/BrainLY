e6ee2dab90849383ae9dc27c4ef700aa2eb1ff3f
satish0320 <rexmax2468@gmail.com>
2024-12-18 00:26:18 +0530

    init

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { Home } from "./pages/home"
 import { SignIn } from "./pages/Signin"
 import { SignUp } from "./pages/Signup"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
-   <div>
-    {/* <SignIn /> */}
-   {/* <Home /> */}
-   <SignUp />
-   </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/Signup" element={<SignUp />} />
+      <Route path="/Signin" element={<SignIn />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- Frontend/src/pages/Signup.tsx
+++ Frontend/src/pages/Signup.tsx
@@ -1,16 +1,22 @@
+import { useRef } from "react"
 
 export const SignUp = () => {
 
+  const usernameRef = useRef <HTMLInputElement | null> (null);
+  const passwordRef = useRef <HTMLInputElement | null> (null);
+
   return (
+
+
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-md shadow-lg w-96">
         <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+            <div  className="block text-sm font-medium text-gray-700">
               Username
-            </label>
+            </div>
             <input
-              id="username"
+            ref={usernameRef}
               type="text"
               name="username"
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -19,11 +25,11 @@ export const SignUp = () => {
           </div>
 
           <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            <div className="block text-sm font-medium text-gray-700">
               Password
-            </label>
+            </div>
             <input
-              id="password"
+              ref={passwordRef}
               type="password"
               name="password"
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
